a61c7b60000df50cfc74cdba133b4edd2a360330
AtotheY <sistillianthony@gmail.com>
2015-11-06 03:35:51 -0500

    to merge

--- results.php
+++ results.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,13 +9,11 @@
 		require_once ('TwitterAPIExchange.php');
 		//setting up OAUTH info
 		$settings = array('oauth_access_token' => "962554652-W3uFUXOMI6UYffcRTConjYBDY3QkcbqH6QBjqE0u", 'oauth_access_token_secret' => "DaurNka3j8r9Yc6ALwIxMJdyJZLFGdVhweIRHnosL31Jw", 'consumer_key' => "KdRTI0oBAIk94w7PiofD0cmo7", 'consumer_secret' => "xIN5Musw80Rlip31To4Gnfx4cZUm2KEoH500hkoLoUL7gmil1y");
-
 		// getting user Id's from previous page
 		$userOne = '&screen_name=' . $_POST['user1'];
 		$userTwo = '&screen_name=' . $_POST['user2'];
 		$url1 = 'https://api.twitter.com/1.1/friends/ids.json';
 		$url2 = 'https://api.twitter.com/1.1/friends/ids.json';
-
 		$getfield1 = "?cursor=-1";
 		$getfield1 .=  $userOne . "&count=5000";
 		$getfield2 = "?cursor=-1";
@@ -25,14 +24,27 @@
 		up_errors', 1);
 		error_reporting(E_ALL);
 		$twitter = new TwitterAPIExchange($settings);
-
 		$response1 = $twitter->setGetfield($getfield1)->buildOauth($url1, $requestMethod)->performRequest();
        		$response2 = $twitter->setGetfield($getfield2)->buildOauth($url2, $requestMethod)->performRequest();	
 		$store1 = json_decode($response1, true);
 		$store2 = json_decode($response2, true);
-		var_dump($store1);
-		var_dump($response1);
+		//var_dump($store1);
+		//var_dump($response1);
+		$count = 0;
+		$ids1 = $store1['ids'];
+		$ids2 = $store2['ids'];
+		foreach ($ids1 as $data1) {
+			foreach ($ids2 as $data2)
+			{
+				if ($data1 == $data2)
+				{
+					$count++;
+					//echo $data1. "is equal to" . $data2;
+					//echo "**";
+				}
+			}
+		}
+		echo "You have ".$count." friends in common!";
 	?>
 </body>
 </html>
-
