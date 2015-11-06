95de6214ccb42c8c2e9b4e289287504986e3122f
AtotheY <sistillianthony@gmail.com>
2015-11-06 01:50:38 -0500

    added proper api calls for both user followers, and variables to store, and a var_dump to see if the information is correctly being stored.

--- results.php
+++ results.php
@@ -11,14 +11,14 @@
 		$settings = array('oauth_access_token' => "962554652-W3uFUXOMI6UYffcRTConjYBDY3QkcbqH6QBjqE0u", 'oauth_access_token_secret' => "DaurNka3j8r9Yc6ALwIxMJdyJZLFGdVhweIRHnosL31Jw", 'consumer_key' => "KdRTI0oBAIk94w7PiofD0cmo7", 'consumer_secret' => "xIN5Musw80Rlip31To4Gnfx4cZUm2KEoH500hkoLoUL7gmil1y");
 
 		// getting user Id's from previous page
-		$userOne = '?q=' . $_POST['user1'];
+		$userOne = '?screen_name=' . $_POST['user1'];
 		$userTwo = '?q=' . $_POST['user2'];
 
 		$url1 = 'https://api.twitter.com/1.1/users/search.json';
 		$url2 = 'https://api.twitter.com/1.1/users/search.json';
 
-		$getfield1 = $userOne;
-		$getfield2 = $userTwo;
+		$getfield1 = $userOne . '&count=5000';
+		$getfield2 = $userTwo . '&count=5000';
 		$requestMethod = 'GET';
 
 		$twitter = new TwitterAPIExchange($settings);
@@ -27,6 +27,7 @@
 
 		$store1 = json_decode($response1, true);
 		$store2 = json_decode($response2, true);
+		var_dump($store1);
 	?>
 </body>
 </html>
