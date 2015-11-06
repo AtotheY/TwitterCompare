f1c2949fa46abc6601ddeb800da49f9685bab3d9
AtotheY <sistillianthony@gmail.com>
2015-11-06 02:47:18 -0500

    got friends for both users, cleaned up code, fixed a few bugs

--- results.php
+++ results.php
@@ -6,28 +6,33 @@
 <body>
 	<?php
 		require_once ('TwitterAPIExchange.php');
-
-		// setting up OAuth
+		//setting up OAUTH info
 		$settings = array('oauth_access_token' => "962554652-W3uFUXOMI6UYffcRTConjYBDY3QkcbqH6QBjqE0u", 'oauth_access_token_secret' => "DaurNka3j8r9Yc6ALwIxMJdyJZLFGdVhweIRHnosL31Jw", 'consumer_key' => "KdRTI0oBAIk94w7PiofD0cmo7", 'consumer_secret' => "xIN5Musw80Rlip31To4Gnfx4cZUm2KEoH500hkoLoUL7gmil1y");
 
 		// getting user Id's from previous page
-		$userOne = '?screen_name=' . $_POST['user1'];
-		$userTwo = '?q=' . $_POST['user2'];
+		$userOne = '&screen_name=' . $_POST['user1'];
+		$userTwo = '&screen_name=' . $_POST['user2'];
+		$url1 = 'https://api.twitter.com/1.1/friends/ids.json';
+		$url2 = 'https://api.twitter.com/1.1/friends/ids.json';
 
-		$url1 = 'https://api.twitter.com/1.1/users/search.json';
-		$url2 = 'https://api.twitter.com/1.1/users/search.json';
-
-		$getfield1 = $userOne . '&count=5000';
-		$getfield2 = $userTwo . '&count=5000';
+		$getfield1 = "?cursor=-1";
+		$getfield1 .=  $userOne . "&count=5000";
+		$getfield2 = "?cursor=-1";
+		$getfield2 .= $userTwo . "&count=5000";
 		$requestMethod = 'GET';
-
+		ini_set('display_errors', 1);
+		ini_set('display_start;
+		up_errors', 1);
+		error_reporting(E_ALL);
 		$twitter = new TwitterAPIExchange($settings);
-		$response1 = $twitter->setGetfield($getfieldOne)->buildOauth($url1, $requestMethod)->performRequest();
-        $response2 = $twitter->setGetfield($getfieldTwo)->buildOauth($url2, $requestMethod)->performRequest();
 
+		$response1 = $twitter->setGetfield($getfield1)->buildOauth($url1, $requestMethod)->performRequest();
+       		$response2 = $twitter->setGetfield($getfield2)->buildOauth($url2, $requestMethod)->performRequest();	
 		$store1 = json_decode($response1, true);
 		$store2 = json_decode($response2, true);
 		var_dump($store1);
+		var_dump($response1);
 	?>
 </body>
 </html>
+
